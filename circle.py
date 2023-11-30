57fc32910d2cb036854412fb5454e907cb082105
boat-bold647 <fanihossein647@gmail.com>
2023-11-30 19:10:44 +0330

    add circle class

---
+++ circle.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Circle:
+    def __init__(self, screen, base_font, pos, color, onClick, radius, width=0):
+        self.screen = screen
+        self.base_font = base_font
+        self.color = color
+        self.base_color = color
+        self.pos = pos
+        self._pos = pos
+        self.radius = radius
+        self.width = width
+        self.onClick = onClick
+ 
+    def _render(self):
+        pygame.draw.circle(self.screen, self.color, self.pos, self.radius, self.width)
+    
+    def render(self, event):
+        self._render()
+
+    @property
+    def position(self):
+        return self._pos
