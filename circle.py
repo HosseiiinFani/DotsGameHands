1e1e0a2c3719c6025042a13bef6616d69a0fda82
boat-bold647 <fanihossein647@gmail.com>
2023-12-06 12:55:46 +0330

    add functions to circle class

--- circle.py
+++ circle.py
@@ -1,7 +1,8 @@
 class Circle:
-    def __init__(self, position, color, radius, life_span, created_at):
+    def __init__(self, position, color, radius, life_span, created_at, function):
         self.position = position
         self.color = color
         self.radius = radius
         self.life_span = life_span
         self.created_at = created_at
+        self.function = function
