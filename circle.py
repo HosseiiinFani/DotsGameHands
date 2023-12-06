b15a206289510022740a2f7739af9836de422287
boat-bold647 <fanihossein647@gmail.com>
2023-12-06 12:48:03 +0330

    remodel Circle class

--- circle.py
+++ circle.py
@@ -1,30 +1,7 @@
-import pygame
-
 class Circle:
-    def __init__(self, screen, base_font, pos, color, onClick, radius, width=0):
-        self.screen = screen
-        self.base_font = base_font
+    def __init__(self, position, color, radius, life_span, created_at):
+        self.position = position
         self.color = color
-        self.base_color = color
-        self.pos = pos
-        self._pos = pos
-        self._radius = radius
-        self.width = width
-        self._onClick = onClick
- 
-    def _render(self):
-        pygame.draw.circle(self.screen, self.color, self.pos, self._radius, self.width)
-    
-    def render(self):
-        self._render()
-
-    def onClick(self):
-        self._onClick()
-
-    @property
-    def position(self):
-        return self._pos
-
-    @property 
-    def radius(self):
-        return self._radius
+        self.radius = radius
+        self.life_span = life_span
+        self.created_at = created_at
